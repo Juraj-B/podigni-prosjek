a303044efdda3142c7c9a0c8f52cad0337b503d9
Gabriel <68819302+obsidianical@users.noreply.github.com>
2022-02-19 00:34:07 +0100

    started with layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<template lang="pug">
+#control
+	textarea( v-model="values" )#grades
+	#inputs
+		#current
+		input( type="text" )#wanted
+		button#calculate Calculate
+#results
+	h1 Possibilities:
+	ul
+		li Result 1
+		li Result 2
+		li Result 3
+		li Result 4
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script lang="ts" setup>
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+	width: 60vw
+	height: 60vh
+	@include debug
+	#control
+		text-area
+		@include debug
+		#inputs
+			#current
+			input
+			#calculate
+	#results
+		h1
+		ul
+			li
+
+*
+	margin: 0
+	padding: 0
+	box-sizing: border-box
+
+html, body
+	height: 100vh
+
+@mixin debug()
+	border: 1px solid red
+
+body
+	display: flex
+	align-items: center
+	justify-content: center
+
 </style>
